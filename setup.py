b0fae801854bc1916642e2b78ac19c07d8d68bd4
dorcas-1404 <dorcasakalo@gmail.com>
2023-06-16 01:04:38 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is a binary classificaton problem to predict if a customer would be happy or not based on feedbacks on survey after service.',
+    author='Dorcas Taiwo',
+    license='MIT',
+)
